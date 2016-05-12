c0cd396ab44edd018a65a7a1a953c47ee653c24d
Zhang张政明 <zhang.zhengming@immomo.com>
2016-05-12 15:30:20 +0800

    updated

--- template/config.js
+++ template/config.js
@@ -9,7 +9,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, 'dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
-    productionSourceMap: true
+    productionSourceMap: false
   },
   dev: {
     port: 8080,
